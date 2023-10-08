2cb5ef6cf54107085d5a54de3f85e4b431898048
Anwar Knyane <21956329+anvarknian@users.noreply.github.com>
2023-10-08 13:53:43 -0300

    Update db.py

--- app/celery/db.py
+++ app/celery/db.py
@@ -8,30 +8,24 @@ from app.config import DB_CONFIG, POOL_SIZE
 
 logger = logging.getLogger(__name__)
 
-
 class DatabaseConnection:
     def __init__(self):
-        logger.info("Initializing connector")
         self.pool_size = POOL_SIZE
         self.pool_name = "globant_pool"
         self.db_config = DB_CONFIG
         self.conn = self.connect()
 
     def connect(self) -> Optional[PooledMySQLConnection]:
-
         try:
-
             conn = mysql.connector.connect(
                 pool_name=self.pool_name,
                 pool_size=self.pool_size,
                 **self.db_config
             )
-            logger.info("Connection pool created successfully")
-
+            logger.info("Connected to the database successfully")
             return conn
-
         except mysql.connector.Error as e:
-            logger.error("Error:", e)
+            logger.error(f"Error connecting to the database: {e}")
             return None
 
     def is_connected(self) -> bool:
@@ -39,6 +33,6 @@ class DatabaseConnection:
 
     def check_connection(self):
         logger.info("Checking connection")
-        if self.is_connected() is False:
+        if not self.is_connected():
             logger.info("Connection lost, Reconnecting...")
-            self.connect()
+            self.conn = self.connect()
